70055b4242ead2551b19ea2192298cc1293fcc67
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2023-10-07 11:30:55 +0200

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import CalculatorScreen from './src/screens/CalculatorScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <CalculatorScreen />
+    </SafeAreaView>
+  );
+};
+
+export default App;
