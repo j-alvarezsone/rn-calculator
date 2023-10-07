deefddf8ee855459eb9203290f9110212361292b
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2023-10-07 12:22:29 +0200

    created global style theme + dark status bar

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {styles} from '../theme/appTheme';
 
 const CalculatorScreen = () => {
   return (
     <View>
-      <Text>CalculatorScreen</Text>
+      <Text style={styles.text}>CalculatorScreen</Text>
     </View>
   );
 };
