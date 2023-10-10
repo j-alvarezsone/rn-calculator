88d4b5a25d469159951a803f95e521cc7be33afa
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2023-10-10 09:23:51 +0200

    added btnOperation fn

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,14 +1,26 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {Text, View} from 'react-native';
 import CalcButton from '../components/CalcButton';
 import {styles} from '../theme/appTheme';
+
+const OPERATORS = {
+  PLUS: '+',
+  MINUS: '-',
+  MULTIPLY: '*',
+  DIVIDE: '/',
+} as const;
+
+type ObjectValues<T> = T[keyof T];
 
 const CalculatorScreen = () => {
   const [number, setNumber] = useState('0');
   const [prevNumber, setPrevNumber] = useState('0');
 
+  const finalOperation = useRef('0');
+
   const clear = () => {
     setNumber('0');
+    setPrevNumber('0');
   };
 
   const buildNumber = (numberText: string) => {
@@ -56,9 +68,25 @@ const CalculatorScreen = () => {
     }
   };
 
+  const changeNumberForPrevious = () => {
+    if (number.endsWith('.')) {
+      setPrevNumber(number.slice(0, -1));
+    } else {
+      setPrevNumber(number);
+    }
+    setNumber('0');
+  };
+
+  const btnOperation = (operation: ObjectValues<typeof OPERATORS>) => {
+    changeNumberForPrevious();
+    finalOperation.current = operation;
+  };
+
   return (
     <View style={styles.calculatorContainer}>
-      <Text style={styles.smallResult}>{prevNumber}</Text>
+      {prevNumber !== '0' && (
+        <Text style={styles.smallResult}>{prevNumber}</Text>
+      )}
       <Text style={styles.result} numberOfLines={1} adjustsFontSizeToFit>
         {number}
       </Text>
@@ -67,34 +95,54 @@ const CalculatorScreen = () => {
         <CalcButton text="C" bgColor="#9B9B9B" action={clear} />
         <CalcButton text="+/-" bgColor="#9B9B9B" action={positiveNegative} />
         <CalcButton text="del" bgColor="#9B9B9B" action={btnDelete} />
-        <CalcButton text="/" bgColor="#FF9427" action={clear} />
+        <CalcButton
+          text="/"
+          bgColor="#FF9427"
+          action={changeNumberForPrevious}
+        />
       </View>
 
       <View style={styles.row}>
         <CalcButton text="7" action={buildNumber} />
         <CalcButton text="8" action={buildNumber} />
         <CalcButton text="9" action={buildNumber} />
-        <CalcButton text="X" action={buildNumber} bgColor="#FF9427" />
+        <CalcButton
+          text="X"
+          action={() => btnOperation(OPERATORS.MULTIPLY)}
+          bgColor="#FF9427"
+        />
       </View>
 
       <View style={styles.row}>
         <CalcButton text="4" action={buildNumber} />
         <CalcButton text="5" action={buildNumber} />
         <CalcButton text="6" action={buildNumber} />
-        <CalcButton text="-" bgColor="#FF9427" action={clear} />
+        <CalcButton
+          text="-"
+          bgColor="#FF9427"
+          action={() => btnOperation(OPERATORS.MINUS)}
+        />
       </View>
 
       <View style={styles.row}>
         <CalcButton text="1" action={buildNumber} />
         <CalcButton text="2" action={buildNumber} />
         <CalcButton text="3" action={buildNumber} />
-        <CalcButton text="+" bgColor="#FF9427" action={clear} />
+        <CalcButton
+          text="+"
+          bgColor="#FF9427"
+          action={() => btnOperation(OPERATORS.PLUS)}
+        />
       </View>
 
       <View style={styles.row}>
         <CalcButton text="0" action={buildNumber} wide />
         <CalcButton text="." action={buildNumber} />
-        <CalcButton text="/" bgColor="#FF9427" action={clear} />
+        <CalcButton
+          text="/"
+          bgColor="#FF9427"
+          action={() => btnOperation(OPERATORS.DIVIDE)}
+        />
       </View>
     </View>
   );
